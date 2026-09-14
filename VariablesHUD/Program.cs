5fe4cfea10c63444fcde2b61e54fc84973f1f93f
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-09-14 10:15:31 -0300

    Hud
    added a Hud method and mimic game play

--- VariablesHUD/Program.cs
+++ VariablesHUD/Program.cs
@@ -2,28 +2,65 @@
 
 namespace VariablesHUD
 {
-    //int -no decimals 
-    //float -decimals f
-    //string -"words you use"
+    //int -whole numbers
+    //float -
+    //string -"words you use" array of characters
     //char -A,B,1,2,$,%
     //bool -true/false
     //var ???
 
     public class Program
     {
+        //Declaration
+        static string gameName = "Game Name !";
+        static int health = 100;
+        static int lives = 3;
+        
+
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
-            //HUD();
+            //Console.WriteLine(health);
+            HUD();
+            Console.ReadKey();
+            //Console.Clear();
+
+            lives = 2;
+            health = 60;
+            HUD();
+            Console.ReadKey();
+            //Console.Clear();
+
+            lives = 1;
+            health = 40;
+            HUD();
+            Console.ReadKey();
+            //Console.Clear();
+
+            lives = 0;
+            health = 0;
+            HUD();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You Died :(");
             Console.ReadKey();
             //Console.Clear();
         }
 
         static void HUD()
         {
+            Console.WriteLine("-----------");
             //game title
+            Console.WriteLine(gameName);
+            Console.WriteLine("");
             //Health
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Health:" + health);
+            Console.WriteLine("");
             //Lives
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Lives:" + lives);
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+
         }
     }
 }
