f1649fd598cb959a9d6497422852964f4790f17d
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-09-14 08:53:09 -0300

    Hello World
    Hello World

--- VariablesHUD/Program.cs
+++ VariablesHUD/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace VariablesHUD
+{
+    //int -no decimals 
+    //float -decimals f
+    //string -"words you use"
+    //char -A,B,1,2,$,%
+    //bool -true/false
+    //var ???
+
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            //HUD();
+            Console.ReadKey();
+            //Console.Clear();
+        }
+
+        static void HUD()
+        {
+            //game title
+            //Health
+            //Lives
+        }
+    }
+}
